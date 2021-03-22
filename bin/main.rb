7fb1fd0005b6199b0bb36e2c314ea276144dcc0a
Promise Johnson <chiemelapromise30@gmail.com>
2021-03-22 19:31:47 +0000

    Add the very basic implementation of web scraping using nokogiri as specified for day 1

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,12 @@
-puts 'hello world'
+#! /usr/bin/env ruby
+require 'nokogiri'
+require 'open-uri'
+
+net = URI.open('https://thenationonlineng.net/')
+
+doc = Nokogiri::HTML(net)
+
+doc.css('.jeg_block_container .jeg_posts_wrap .jeg_posts article .jeg_postblock_content h3 a').each do |link|
+  p link.inner_text
+end
+
