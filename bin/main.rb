96f535cb88000909b1c38c0f3a2ff3b798e7fae8
Promise Johnson <chiemelapromise30@gmail.com>
2021-03-22 21:31:19 +0000

    Fix the functionality

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,8 @@
-#!/usr/bin/env ruby
 require 'nokogiri'
 require 'open-uri'
 
-net = URI.open('https://thenationonlineng.net')
+net = URI.open('https://traversymedia.com')
 
-doc = Nokogiri::HTML(net)
+p doc = Nokogiri::HTML(net)
 
-doc.css('.jeg_block_container .jeg_posts_wrap .jeg_posts article .jeg_postblock_content h3 a').each do |link|
-  p link.inner_text
-end
+
