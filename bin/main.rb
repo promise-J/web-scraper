a6defb83f571070e3e64596b41e038b1709d095f
Promise Johnson <chiemelapromise30@gmail.com>
2021-03-24 20:14:20 +0000

    Fix the linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -5,48 +5,50 @@ require_relative '../lib/scraper'
 WEB = Scraper.new('https://www.traversymedia.com/')
 
 def getting_started
-WEB.welcome
-puts "Are you ready to scrape? ENTER yes or no"
-choice = gets.chomp
-
-until choice == 'yes'
-  puts "enter a valid choice. to scrape Enter yes or no"
+  WEB.welcome
+  puts 'Are you ready to scrape? ENTER yes or no'
   choice = gets.chomp
-end
 
-WEB.about
-WEB.how_to_go
-ready = true
-
-while ready
-  puts 'Choose an option to scrape from the Traversy'
-  scraper_choice  = gets.chomp.to_i
-  case scraper_choice
-  when 1
-   puts  WEB.get_titles
-  when 2
-   p  WEB.course_1
-  when 3
-    p WEB.course_2
-  else
-    puts WEB.error_input
+  until choice == 'yes'
+    puts 'enter a valid choice. to scrape Enter yes or no'
+    choice = gets.chomp
   end
-  ready = false
+
+  WEB.about
+  WEB.how_to_go
+  confirm
 end
+
+def confirm
+  ready = true
+  while ready
+    puts 'Choose an option to scrape from the Traversy'
+    scraper_choice = gets.chomp.to_i
+    case scraper_choice
+    when 1
+      puts WEB.fetch_title
+    when 2
+      p WEB.course1
+    when 3
+      p WEB.course2
+    else
+      puts WEB.error_input
+    end
+    ready = false
+  end
 end
 
 re_do = true
 
 while re_do
   getting_started
-puts 'Do you want to scrape again? choose [yes, no]'
-choice = gets.chomp
+  puts 'Do you want to scrape again? choose [yes, no]'
+  choice = gets.chomp
 
-until choice == 'yes'
-  puts 'oooop!!! seem you dont want to scrape again. Lets scrape some other time.'
-  re_do = false
-  WEB.end_scrape
+  until choice == 'yes'
+    puts 'oooop!!! seem you dont want to scrape again. Lets scrape some other time.'
+    re_do = false
+    WEB.end_scrape
+  end
+  getting_started
 end
-getting_started
-end
-
--- lib/scraper_instr.rb
+++ lib/scraper_instr.rb
@@ -1,26 +1,32 @@
-class Scraper_play
-    attr_reader :url
-    def initialize(url)
-      @url = url
-    end
-    def welcome
-      puts "You are welcomed to web scrapping. Let's scrap #{@url}"
-    end
-    def about
-      puts "This is The Traversy Media official website. This consist of a whole lots of courses to learn Programming."
-    end
-    def how_to_go
-      puts "enter: 
+class ScraperPlay
+  attr_reader :url
+
+  def initialize(url)
+    @url = url
+  end
+
+  def welcome
+    puts "You are welcomed to web scrapping. Let's scrap #{@url}"
+  end
+
+  def about
+    puts 'This is The Traversy Media official website. This consist of a whole lots of courses to learn Programming.'
+  end
+
+  def how_to_go
+    puts "enter:
       1-- To see the list of the categories of courses availabe
       2-- To see the list of the most popular web courses
       3-- To see the list of all youtube crash courses.
       another other option would exit.
       "
-    end
-    def error_input
-      puts "Your selection is invalid, try again."
-    end
-    def end_scrape
-      puts "Scrape ended. Scrape again later"
-    end   
+  end
+
+  def error_input
+    puts 'Your selection is invalid, try again.'
+  end
+
+  def end_scrape
+    puts 'Scrape ended. Scrap again later'
+  end
 end
